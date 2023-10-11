7e54a4afd7412d985779fcb6007e7ee952a84420
Kaique A <42048798+Kaique292@users.noreply.github.com>
2023-10-11 13:22:39 -0300

    change indent size

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,30 @@
 import { NativeModules, Platform } from 'react-native';
 
 type Location = {
-    latitude: number;
-    longitude: number;
-    altitude: number;
+  latitude: number;
+  longitude: number;
+  altitude: number;
 }
 
 type AndroidMockLocationType = {
-    multiply(a: number, b: number): Promise<number>;
-    setTestProviderLocation(useGPS: boolean, altitude: number, latitude: number, longitude: number): void;
-    getMockLocation(useGPS: boolean): Promise<any>;
-    stopMockLocation(): void;
-    checkLocationPermission(): Promise<any>;
-    requestLocationPermission(): Promise<any>;
+  multiply(a: number, b: number): Promise<number>;
+  setTestProviderLocation(useGPS: boolean, altitude: number, latitude: number, longitude: number): void;
+  getMockLocation(useGPS: boolean): Promise<any>;
+  stopMockLocation(): void;
+  checkLocationPermission(): Promise<any>;
+  requestLocationPermission(): Promise<any>;
 }
 
 const LINKING_ERROR =
-    `The package 'react-native-android-mock-location' doesn't seem to be linked. Make sure: \n\n` +
-    Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-    '- You rebuilt the app after installing the package\n' +
-    '- You are not using Expo Go\n';
+  `The package 'react-native-android-mock-location' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
 
 const onModuleError = () => new Proxy({}, {
-    get() {
-        throw new Error(LINKING_ERROR);
-    }
+  get() {
+    throw new Error(LINKING_ERROR);
+  }
 })
 
 const AndroidMockLocation: AndroidMockLocationType = NativeModules.AndroidMockLocation ? NativeModules.AndroidMockLocation : onModuleError();
@@ -32,34 +32,34 @@ const AndroidMockLocation: AndroidMockLocationType = NativeModules.AndroidMockLo
 
 
 function multiply(a: number, b: number): Promise<number> {
-    return AndroidMockLocation.multiply(a, b);
+  return AndroidMockLocation.multiply(a, b);
 }
 
 function setMockLocation(useGPS: boolean, location: Location) {
-    return AndroidMockLocation.setTestProviderLocation(useGPS, location.altitude, location.latitude, location.longitude);
+  return AndroidMockLocation.setTestProviderLocation(useGPS, location.altitude, location.latitude, location.longitude);
 }
 
 function getMockLocation(useGPS: boolean) {
-    return AndroidMockLocation.getMockLocation(useGPS);
+  return AndroidMockLocation.getMockLocation(useGPS);
 }
 
 function stopMockLocation() {
-    return AndroidMockLocation.stopMockLocation();
+  return AndroidMockLocation.stopMockLocation();
 }
 
 function checkLocationPermission() {
-    return AndroidMockLocation.checkLocationPermission();
+  return AndroidMockLocation.checkLocationPermission();
 }
 
 function requestLocationPermission() {
-    return AndroidMockLocation.requestLocationPermission();
+  return AndroidMockLocation.requestLocationPermission();
 }
 
 export {
-    multiply,
-    setMockLocation,
-    getMockLocation,
-    stopMockLocation,
-    checkLocationPermission,
-    requestLocationPermission
+  multiply,
+  setMockLocation,
+  getMockLocation,
+  stopMockLocation,
+  checkLocationPermission,
+  requestLocationPermission
 }
